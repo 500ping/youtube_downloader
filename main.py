583f67fb23e6d59f3db5b5d094f41372154144c2
Nguyen Duc Thang <48019863+500ping@users.noreply.github.com>
2023-02-23 17:36:17 +0700

    first init

---
+++ main.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+
+yt = YouTube('https://www.youtube.com/watch?v=h_GGd7HfKQ8')
+videos = list(
+    filter(
+        lambda x: getattr(x, "mime_type", None) == "video/mp4"
+        and getattr(x, "resolution", None) and not getattr(x, "is_progressive", True), yt.streams)
+)
+
+for i, video in enumerate(videos):
+    resolution = getattr(video, "resolution")
+    print(f"{i + 1}: {resolution}")
+
+choice = int(input("Enter resolution: "))
+index = choice - 1
+if index not in range(len(videos)):
+    print("Error")
+else:
+    videos[index].download()
